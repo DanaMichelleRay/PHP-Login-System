f2a691b4f38a18fb23295056929a1347d3eefc64
D.Michelle Ray <BH05102@tn.gov>
2020-04-22 13:30:59 -0500

    Upload of Template

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>PHP Log-in System</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+</head>
+
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/js/uikit-icons.min.js"></script>
+<body>
 <?php echo "Hello World"; ?>
+
+</body>
+</html>
